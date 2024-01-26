fe8686fb81f2a3aaceda7efa51466ff7b6c89a50
Renjie Liu <liurenjie2008@gmail.com>
2024-01-26 10:43:48 +0800

    Fix maven

--- java/connector-node/risingwave-sink-iceberg/src/test/java/com/risingwave/connector/catalog/JniCatalogWrapperTest.java
+++ java/connector-node/risingwave-sink-iceberg/src/test/java/com/risingwave/connector/catalog/JniCatalogWrapperTest.java
@@ -1,24 +1,25 @@
 package com.risingwave.connector.catalog;
 
-import org.apache.iceberg.catalog.TableIdentifier;
 import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
 
 public class JniCatalogWrapperTest {
     @Test
     public void testJdbc() throws Exception {
-        JniCatalogWrapper catalog = JniCatalogWrapper.create("risingwave", "org.apache.iceberg.jdbc.JdbcCatalog", new String[]{
-                "uri", "jdbc:postgresql://192.168.148.3:5432/iceberg",
-                "jdbc.user", "admin",
-                "jdbc.password", "123456",
-                "warehouse", "s3://icebergdata/demo",
-                "io-impl", "org.apache.iceberg.aws.s3.S3FileIO",
-                "s3.endpoint", "http://192.168.148.4:9301",
-                "s3.path-style-access", "true",
-                "s3.access-key-id", "hummockadmin",
-                "s3.secret-access-key", "hummockadmin",
-        });
+        JniCatalogWrapper catalog =
+                JniCatalogWrapper.create(
+                        "risingwave",
+                        "org.apache.iceberg.jdbc.JdbcCatalog",
+                        new String[] {
+                            "uri", "jdbc:postgresql://192.168.148.3:5432/iceberg",
+                            "jdbc.user", "admin",
+                            "jdbc.password", "123456",
+                            "warehouse", "s3://icebergdata/demo",
+                            "io-impl", "org.apache.iceberg.aws.s3.S3FileIO",
+                            "s3.endpoint", "http://192.168.148.4:9301",
+                            "s3.path-style-access", "true",
+                            "s3.access-key-id", "hummockadmin",
+                            "s3.secret-access-key", "hummockadmin",
+                        });
 
         catalog.loadTable("s1.t1");
     }
